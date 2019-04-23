24526aa20f0adffce33ff037dc04049161a80693
Dmitrii Beliakov <dbeliakov@yandex-team.ru>
2019-04-23 08:58:51 +0300

    Seminar 20

---
+++ seminars/20/README.md
@@ -0,0 +1,8 @@
+# Семинар 20. Сокеты
+
+[Презентация](https://dbeliakov.github.io/hse-os-2018/seminars/20/slides/)  
+[Примеры кода](code)
+
+### Дополнительные материалы
+* [Материалы прошлого года](https://github.com/hseos/hseos-course/tree/master/2017/20-socket)
+* [Про работу с сетью в Linux](http://masandilov.ru/network/guide_to_network_programming5)
---
+++ seminars/20/code/host/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET_ADDRSTRLEN];
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: %s HOST", argv[0]);
+        return 1;
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %sn", gai_strerror(status));
+        return 2;
+    }
+
+    printf("IP addresses for %s:\n\n", argv[1]);
+
+    for(p = res;p != NULL; p = p->ai_next) {
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+        void* addr = &(ipv4->sin_addr);
+        char* ipver = "IPv4";
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(res); // free the linked list
+
+    return 0;
+}
